b0ad48eb6f7cba20470da4345dbad0247c8b6830
Ihor Petrenko <sabotag1985@gmail.com>
2020-09-06 09:25:30 +0300

    Initial actions
    Install mantis;
    modify sessionhelper fixture
    add new test - login with the known credentials

---
+++ fixture/application.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# вспомогательные методы тестового класса
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.driver = webdriver.Firefox()
+        elif browser == "chrome":
+            self.driver = webdriver.Chrome()
+        elif browser == "ie":
+            self.driver = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.driver.implicitly_wait(2)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+
+    def is_valid(self):
+        try:
+            self.driver.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        driver = self.driver
+        driver.get(self.base_url)
+
+    def destroy(self):
+        self.driver.quit()
---
+++ fixture/session.py
@@ -0,0 +1,44 @@
+class SessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, user, pwd):
+        driver = self.app.driver
+        self.app.open_home_page()  # method
+        driver.find_element_by_name("username").click()
+        driver.find_element_by_name("username").clear()
+        driver.find_element_by_name("username").send_keys(user)
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys(pwd)
+        driver.find_element_by_css_selector('input[type="submit"]').click()
+
+    def logout(self):
+        driver = self.app.driver
+        driver.find_element_by_link_text("Logout").click()
+
+    def ensure_logout(self):
+        driver = self.app.driver
+        if self.is_logged_in():
+            self.logout()
+
+    def ensure_login(self, user, pwd):
+        driver = self.app.driver
+        if self.is_logged_in():
+            if self.is_logged_in_as(user):
+                return
+            else:
+                self.logout()
+        self.login(user, pwd)
+
+    def is_logged_in(self):
+        driver = self.app.driver
+        return len(driver.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, user):
+        driver = self.app.driver
+        return self.get_logged_user() == user
+
+    def get_logged_user(self):
+        driver = self.app.driver
+        return driver.find_element_by_css_selector("td.login-info-left span").text
